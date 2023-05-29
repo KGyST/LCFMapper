b4a8f4bc59a7bbfb72d206f06dd84512d4730975
Sámuel Gyula KARLI <karli.samu@gmail.com>
2023-05-29 17:02:18 +0200

    SamUI functions removed XLSX DI stuff

--- LCFMapper.py
+++ LCFMapper.py
@@ -35,6 +35,7 @@ except ImportError:
     from lxml import etree
 
 from GSMXMLLib import *
+from SamUITools import singleton, CreateToolTip, InputDirPlusText
 
 # FIXME Enums
 ID = ''
@@ -97,32 +98,41 @@ _P_ = 15;   _Q_ = 16;   _R_ = 17;   _S_ = 18;   _T_ = 19
 _U_ = 20;   _V_ = 21;   _W_ = 22;   _X_ = 23;   _Y_ = 24
 _Z_ = 25
 
-import openpyxl as opx
+
+class XLSXLoader():
+    def __init__(self, table_url:str):
+        import openpyxl as opx
+        self.wb = opx.load_workbook(table_url)
+
+    def __getitem__(self, sheet_name:str):
+        return [cell.value for cell in [row for row in self.wb[sheet_name]]]
 
 
 class ParamMapping:
-    def __init__(self, p_iType, p_row):
+    def __init__(self, p_iType:int, p_row):
         self._type = p_iType
-        self._files = str.split(p_row[_A_].value, ";") if p_row[_A_].value else []
-        self._paramName = p_row[_B_].value
-        self._paramDesc = p_row[_C_].value
-        self._from = p_row[_D_].value
-        self._to = p_row[_F_].value
+        self._files = str.split(p_row[_A_], ";") if p_row[_A_] else []
+        self._paramName = p_row[_B_]
+        self._paramDesc = p_row[_C_]
+        self._from = p_row[_D_]
+        self._to = p_row[_F_]
 
 
 class ParamMappingContainer:
-    def __init__(self, p_sXLSX):
+    def __init__(self, p_sXLSX:str):
         # FIXME Google Spreadsheet, too
         self._mappingList = []
+        self.loader = XLSXLoader(p_sXLSX)
 
-        wb = opx.load_workbook(p_sXLSX.get())
         for _sheetName, _paramType in PARAM_TYPES.items():
             try:
-                _sheet = wb[_sheetName]
+                _sheet = self.loader[_sheetName]
             except KeyError:
                 continue
-            for row in _sheet.iter_rows(min_row=2):
+
+            for row in _sheet[1:]:
                 self._mappingList.append(ParamMapping(_paramType, row))
+
 
     def applyParams(self, p_parSect, p_fileName):
         for mapping in self._mappingList:
@@ -133,93 +143,6 @@ class ParamMappingContainer:
 
 
 #----------------- gui classes -----------------------------------------------------------------------------------------
-
-class CreateToolTip:
-    def __init__(self, widget, text='widget info'):
-        self.waittime = 500
-        self.wraplength = 180
-        self.widget = widget
-        self.text = text
-
-        self.widget.bind("<Enter>", self.enter)
-        self.widget.bind("<Leave>", self.leave)
-        self.widget.bind("<ButtonPress>", self.leave)
-        self.id = None
-        self.tw = None
-
-    def enter(self, event=None):
-        self.schedule()
-
-    def leave(self, event=None):
-        self.unschedule()
-        self.hidetip()
-
-    def schedule(self):
-        self.unschedule()
-        self.id = self.widget.after(self.waittime, self.showtip)
-
-    def unschedule(self):
-        idx = self.id
-        self.id = None
-        if idx:
-            self.widget.after_cancel(idx)
-
-    def showtip(self, event=None):
-        x, y, cx, cy = self.widget.bbox("insert")
-        x += self.widget.winfo_rootx() + 25
-        y += self.widget.winfo_rooty() + 20
-        # creates a toplevel window
-        self.tw = tk.Toplevel(self.widget)
-        # Leaves only the label and removes the app window
-        self.tw.wm_overrideredirect(True)
-        self.tw.wm_geometry("+%d+%d" % (x, y))
-        label = tk.Label(self.tw, text=self.text, justify='left',
-                       background="#ffffff", relief='solid', borderwidth=1,
-                       wraplength = self.wraplength)
-        label.pack(ipadx=1)
-
-    def hidetip(self):
-        tw = self.tw
-        self.tw = None
-        if tw:
-            tw.destroy()
-
-
-class InputDirPlusText:
-    def __init__(self, top, text, target, tooltip='', row=0, column=0, func=tkinter.filedialog.askdirectory, title="Select folder"):
-        self.target = target
-        self.filename = ''
-        self._frame = tk.Frame(top)
-        self._frame.grid({"row": row, "column": column})
-
-        self._frame.columnconfigure(1, weight=1)
-
-        self.buttonDirName = tk.Button(self._frame, {"text": text, "command": self.getFunc(func, title) })
-        self.buttonDirName.grid({"sticky": tk.W + tk.E, "row": 0, "column": 0, })
-
-        self.entryName = tk.Entry(self._frame, {"width": 30, "textvariable": target})
-        self.entryName.grid({"row": 0, "column": 1, "sticky": tk.E + tk.W, })
-
-        if tooltip:
-            CreateToolTip(self._frame, tooltip)
-
-    def getFunc(self, func, title):
-        def inputDirName():
-            self.filename = func(initialdir="/", title=title)
-            self.target.set(self.filename)
-            self.entryName.delete(0, tk.END)
-            self.entryName.insert(0, self.filename)
-        return inputDirName
-
-
-# https://stackoverflow.com/questions/12305142/issue-with-singleton-python-call-two-times-init
-def singleton(cls):
-    instances = {}
-    def getinstance():
-        if cls not in instances:
-            instances[cls] = cls()
-        return instances[cls]
-    return getinstance
 
 
 @singleton
@@ -502,7 +425,7 @@ class GUIAppSingleton(tk.Frame):
 def processOneXML(p_Data):
     dest = p_Data['dest']
     tempDir = p_Data["tempDir"]
-    mapping = ParamMappingContainer(GUIAppSingleton().SourceXLSXPath)
+    mapping = ParamMappingContainer(GUIAppSingleton().SourceXLSXPath.get())
 
     src = dest.sourceFile
     srcPath = src.fullPath
