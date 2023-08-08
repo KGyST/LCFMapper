c64828e00835065130eec43921093e594a46bd20
Sámuel Gyula KARLI <karli.samu@gmail.com>
2023-08-08 23:13:40 +0200

    to stack overflow

--- check_async_mp.py
+++ check_async_mp.py
@@ -1,76 +1,54 @@
-import asyncio
 import multiprocessing
-import tkinter as tk
+import os
+import time
+import asyncio
+import random
 
+N_PROCESSES = 2
+N_ITER = 10
+N_SEC = 1
 
-class Loop:
-    """
-    A simple event loop class for tkinter applications.
-    """
+async_queue = asyncio.Queue()
 
-    def __init__(self, top):
-        self.top = top
-        self._loop = asyncio.get_event_loop()
-        self._loop.create_task(self.asyncio_event_loop())
+def worker_main(p_queue):
+    print (_pid:=os.getpid(),"working")
+    for i in range(N_ITER):
+        some_random_time = N_SEC * random.random()
+        p_queue.put(f"{i} - {_pid}: {some_random_time} sec")
+        time.sleep(some_random_time)
+    p_queue.put(None)
 
-    async def asyncio_event_loop(self, interval=0):
-        while True:
-            self.top.update()
-            await asyncio.sleep(interval)
-            _task = asyncio.current_task()
-            if _task:
-                if _task.cancelled():
-                    raise asyncio.CancelledError()
+async def run():
+    await mp_queue_to_async_queue()
+    await print_out_async()
 
-    def __getattr__(self, item):
-        return getattr(self._loop, item)
+async def mp_queue_to_async_queue():
+    processes_finished = 0
+    while True:
+        message = mp_queue.get()
+        print(f"-> {message}")
+        await async_queue.put(message)
+        if message == None:
+            processes_finished += 1
+        if processes_finished == N_PROCESSES:
+            break
 
+async def print_out_async():
+    processes_finished = 0
+    while True:
+        b = await async_queue.get()
+        print(f"<- {b}")
+        if b == None:
+            processes_finished += 1
+        if processes_finished == N_PROCESSES:
+            break
 
-class Client(tk.Frame):
-    def __init__(self, server_queue):
-        super().__init__()
-        self.top = self.winfo_toplevel()
-        self.loop = Loop(self.top)
+if __name__ == '__main__':
+    mp_queue = multiprocessing.Queue()
+    pool = multiprocessing.Pool(processes=N_PROCESSES, initializer=worker_main, initargs=(mp_queue,))
 
-        self.text = tk.Text()
-        self.text.pack()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
 
-        self.server_queue = server_queue  # Change the type of the server_queue attribute
+    pool.terminate()
 
-    def mainloop(self):
-        # server.start()  # Start the server process before starting the client process
-        self.loop.create_task(self._get_message())   # Get the message from the queue
-        self.loop.run_forever()
-
-    async def _get_message(self):
-        while True:
-            message = await self.server_queue.get()  # Get the message from the server_queue
-            self.text.insert("end", f"{message}\n")  # Display the message in the client's text widget
-
-
-class Server(multiprocessing.Process):
-    def __init__(self):
-        super().__init__()
-        self._server_queue = asyncio.Queue()
-        self.messages = ["Hello, world!", "This is a message from the server.", "Have a nice day!"]
-
-    def run(self):
-        loop = asyncio.get_event_loop()
-        loop.create_task(self._run())
-
-    async def _run(self):
-        while True:
-            for message in self.messages:
-                await self._server_queue.put(message)  # Put the message in the server_queue
-                await asyncio.sleep(1)  # Wait 1 second before sending the next message
-
-    @property
-    def server_queue(self):
-        return self._server_queue
-
-
-if __name__ == "__main__":
-    server = Server()
-    client = Client(server.server_queue)
-    server.run()
-    client.mainloop()
