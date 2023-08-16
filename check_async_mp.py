737ec537835888005205f0d2378b4acffbe325f8
Sámuel Gyula KARLI <karli.samu@gmail.com>
2023-08-17 00:04:39 +0200

    check_async_mp.py seems to work with multiprocessing.Pool

--- check_async_mp.py
+++ check_async_mp.py
@@ -3,32 +3,42 @@ import os
 import time
 import asyncio
 import random
+from concurrent.futures import ProcessPoolExecutor
+from queue import Empty as QueueEmpty
 
-N_PROCESSES = 2
-N_ITER = 10
+N_PROCESSES = 8
+N_ITER = 20
 N_SEC = 1
 
 async_queue = asyncio.Queue()
 
-def worker_main(p_queue):
-    print (_pid:=os.getpid(),"working")
-    for i in range(N_ITER):
-        some_random_time = N_SEC * random.random()
-        p_queue.put(f"{i} - {_pid}: {some_random_time} sec")
-        time.sleep(some_random_time)
-    p_queue.put(None)
+def worker_main(p_item):
+    mp_queue.put(f"{p_item[0]} - {os.getpid()}: {p_item[1]} sec")
+    time.sleep(p_item[1])
 
-async def run():
-    await mp_queue_to_async_queue()
+def init_worker(queue):
+    global mp_queue
+    mp_queue = queue
+    print(os.getpid(), "working")
+
+async def run(mp_queue):
+    asyncio.create_task(mp_queue_to_async_queue(mp_queue))  # Pass mp_queue as an argument
     await print_out_async()
 
-async def mp_queue_to_async_queue():
+
+async def mp_queue_to_async_queue(mp_queue):  # Receive mp_queue as an argument
     processes_finished = 0
     while True:
-        message = mp_queue.get()
+        try:
+            message = mp_queue.get_nowait()
+        except QueueEmpty:
+            await asyncio.sleep(0)
+            continue
+        except BrokenPipeError:
+            break
         print(f"-> {message}")
         await async_queue.put(message)
-        if message == None:
+        if message is None:
             processes_finished += 1
         if processes_finished == N_PROCESSES:
             break
@@ -38,17 +48,24 @@ async def print_out_async():
     while True:
         b = await async_queue.get()
         print(f"<- {b}")
-        if b == None:
-            processes_finished += 1
-        if processes_finished == N_PROCESSES:
+        processes_finished += 1
+        if processes_finished == N_ITER:
             break
 
-if __name__ == '__main__':
-    mp_queue = multiprocessing.Queue()
-    pool = multiprocessing.Pool(processes=N_PROCESSES, initializer=worker_main, initargs=(mp_queue,))
+def worker_pool(mp_queue):  # Pass mp_queue as an argument
+    pool_map = [(i, N_SEC * random.random()) for i in range(N_ITER)]
+    with ProcessPoolExecutor(max_workers=N_PROCESSES, initializer=init_worker, initargs=(mp_queue,)) as executor:
+        executor.map(worker_main, pool_map)
 
+async def main():
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
 
-    pool.terminate()
+    with multiprocessing.Manager() as manager:
+        mp_queue = manager.Queue()
+        _task = loop.create_task(run(mp_queue))
+        loop.run_in_executor(None, worker_pool, mp_queue)  # Pass mp_queue as an argument
 
+        await _task
+
+if __name__ == '__main__':
+    asyncio.run(main())
